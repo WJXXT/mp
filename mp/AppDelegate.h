9052778c22b3e940df570b84e19e47776e39279a
WJXXT <wjxxt@hotmail.com>
2015-05-16 00:27:43 +0800

    mp
    hehe

---
+++ mp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mp
+//
+//  Created by 王新国 on 15/5/15.
+//  Copyright (c) 2015年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mp
+//
+//  Created by 王新国 on 15/5/15.
+//  Copyright (c) 2015年 XXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
